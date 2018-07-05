1e8ff8b63956b68e2aeefc3d78e34b5b6b98de48
David Hopkins <dlhopkins2@gmail.com>
2018-07-05 17:38:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function distanceFromHqInBlocks(distanceBlocks) {
   return Math.abs(42 - distanceBlocks)
 }
+
+function distanceFromHqInFeet(distanceFeet) {
+  return distanceFromHqInBlocks * 264
+}
