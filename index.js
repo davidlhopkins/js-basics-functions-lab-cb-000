59d326ad9c02647de2a88dcdfd0cfedef9b44894
David Hopkins <dlhopkins2@gmail.com>
2018-07-05 17:52:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ function distanceFromHqInFeet(distanceBlocks) {
   return distanceFromHqInBlocks(distanceBlocks) * 264
 }
 
-function distanceTravelledInFeet(start, end) {
-  return Math.abs(end - start) * 264
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  let distanceInFeet = distanceTravelledInFeet(start, destination)
+
+  if (distanceInFeet <= 400) {
+    fare = 0
+  }
+  else if (400 < distanceInFeet <= 2000) {
+    fare = distanceInFeet * 0.02
+  }
+  else if (2000 < distanceInFeet <= 2500) {
+    fare = 25
+  }
+  else if (2500 < distanceInFeet) {
+    fare = 'cannot travel that far'
+  }
+  return fare
 }
